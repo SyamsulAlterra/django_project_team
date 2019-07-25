3b02c8dc06afa67d3f070cf93f4c8a19574baec1
Syamsul <syamsul@alterra.id>
2019-07-25 17:35:58 +0700

    base and homepage

--- website/views.py
+++ website/views.py
@@ -1,3 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .forms import *
 
-# Create your views here.
+def homepage(request):
+    return render(request, 'homepage.html')
