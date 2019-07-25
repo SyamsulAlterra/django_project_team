423e1332d55ccb9055a9d4a0e3506fe75dad8d20
Syamsul <syamsul@alterra.id>
2019-07-26 00:37:17 +0700

    form places

--- website/urls.py
+++ website/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns=[
     path('', views.homepage, name='homepage'),
     path('result/', views.result, name='result'),#ganti sama path result
+    path('location_form/', views.location_form, name='location form'),
 ]
--- website/views.py
+++ website/views.py
@@ -1,18 +1,22 @@
 from django.shortcuts import render,redirect
 from .forms import *
 
+
 def homepage(request):
     if request.method=='POST':
         string = location_to_search(request.POST)
         if string.is_valid():
+            request.session['string'] = string.cleaned_data
+            # return redirect('result')
             return render(request, 'result.html', {'string':string})
     else:
         string = location_to_search()
 
-    return render(request, 'homepage.html', {'string':string})
+    return render(request, 'homepage.html', {'string':string} )
 
 #fungsi manggil result
 def result(request):
+    request.session.pop(['string'],{})
     if request.method=='POST':
         string = location_to_search(request.POST)
         if string.is_valid():
@@ -20,4 +24,20 @@ def result(request):
     else:
         string = location_to_search()
         
-    return render(request, 'result.html')
+    return render(request, 'result.html', {'string':string})
+
+def location_form(request):
+    if request.method=='POST':
+        data = input_location(request.POST)
+        string = location_to_search(request.POST)
+        if data.is_valid():
+            data.save()
+            string = location_to_search()
+            return redirect('homepage')
+        elif string.is_valid():
+            return redirect('result')
+    else:
+        data = input_location()
+        string = location_to_search()
+
+    return render(request, 'location_form.html', {'data':data, 'string':string})
