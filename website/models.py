c256623e318b4c3ceb8f0c5200c37e265568df51
fikriamri <famri@alterra.id>
2019-07-25 17:24:13 +0700

    add model

--- website/models.py
+++ website/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
 # Create your models here.
+class Places(models.Model):
+    name = models.CharField(max_length=255, help_text="Insert place name")
+    description = models.TextField(max_length=1000, help_text="Insert place descriptions")
+    maps = models.CharField(max_length=500, help_text='Insert html script from google maps')
+
+    def __str__(self):
+        return self.name
+
+class Photos(models.Model):
+    # place_id = models.ForeignKey(Places.id, on_delete=models.CASCADE)
+    place_name = models.ForeignKey(Places, on_delete=models.CASCADE)
+    pic_url = models.CharField(max_length=2000)
+
+    # def __str__(self):
+    #     return self.place_name
+
+
+class Review(models.Model):
+    # place_id = models.ForeignKey(Places.id, on_delete=models.CASCADE)
+    place_name = models.ForeignKey(Places, on_delete=models.CASCADE)
+    review = models.TextField(max_length=500)
+
+    # def __str__(self):
+    #     return self.place_name
