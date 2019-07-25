911a4bc4647f8aad8e8a841e1099b89fd2d78321
fikriamri <famri@alterra.id>
2019-07-26 00:34:17 +0700

    finish search-result

--- website/views.py
+++ website/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render,redirect
 from .forms import *
+from .models import *
 
 def homepage(request):
     if request.method=='POST':
         string = location_to_search(request.POST)
         if string.is_valid():
-            return render(request, 'result.html', {'string':string})
+            target=string.cleaned_data['location']
+            filter_places=Places.objects.all().filter(name__contains=target)
+            return render(request, 'search_result.html', {'places':filter_places, 'string':string})
     else:
         string = location_to_search()
 
@@ -16,8 +19,8 @@ def result(request):
     if request.method=='POST':
         string = location_to_search(request.POST)
         if string.is_valid():
-            return render(request, 'result.html', {'string':string})
+            return render(request, 'search_result.html', {'string':string})
     else:
         string = location_to_search()
         
-    return render(request, 'result.html')
+    return render(request, 'search_result.html')
